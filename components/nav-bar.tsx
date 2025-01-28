637cef0843f61e95b8a4cb17c4a51f7fac362ccd
Almujahid Jamion <94305038+firemelon96@users.noreply.github.com>
2025-01-28 15:47:52 +0800

    fix error

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -14,7 +14,6 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
-import { Home } from 'lucide-react';
 import Image from 'next/image';
 
 const components: { title: string; href: string; description: string }[] = [
@@ -65,7 +64,7 @@ export function Navbar() {
             <ul className='grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
               <li className='row-span-3'>
                 <NavigationMenuLink asChild>
-                  <a
+                  <Link
                     className='flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'
                     href='/'
                   >
@@ -78,7 +77,7 @@ export function Navbar() {
                     <p className='text-sm leading-tight text-muted-foreground'>
                       Where Journeys Meet the Depths of Discovery.
                     </p>
-                  </a>
+                  </Link>
                 </NavigationMenuLink>
               </li>
               <ListItem href='/docs' title='Introduction'>
