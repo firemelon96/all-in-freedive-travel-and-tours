6014e3a072379cde7beeddaa4934ae9e01250e1f
Almujahid Jamion <94305038+firemelon96@users.noreply.github.com>
2025-02-26 19:30:52 +0800

    removed setting for nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  async redirects() {
-    return [
-      {
-        source: 'http://www.allintravelandfreedivingtourservices.com/',
-        destination: 'https://www.allinfreedivingandtourservices.com/',
-        permanent: true, // 301 redirect
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
