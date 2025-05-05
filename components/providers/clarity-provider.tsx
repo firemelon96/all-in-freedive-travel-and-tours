9e0607ba6ff32e42e804a0b6e9e713b5bc405c09
Almujahid Jamion <94305038+firemelon96@users.noreply.github.com>
2025-05-06 00:04:50 +0800

    added next public to clarity

--- components/providers/clarity-provider.tsx
+++ components/providers/clarity-provider.tsx
@@ -6,7 +6,7 @@ import Clarity from '@microsoft/clarity';
 const ClarityProvider = ({ children }: { children: ReactNode }) => {
   useEffect(() => {
     if (process.env.NODE_ENV === 'production') {
-      Clarity.init(process.env.CLARITY_ID!);
+      Clarity.init(process.env.NEXT_PUBLIC_CLARITY_ID!);
     }
   }, []);
 
